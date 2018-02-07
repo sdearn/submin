23a9d9134b87cc1bb9d423e1c16566e0ca3a08a1
sdearn <31753221+sdearn@users.noreply.github.com>
2018-02-07 11:58:07 +0800

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,38 +10,35 @@ svn_repo="${SUBMIN_SVN_DIR:-/submin/svn}"
 git_repo="${SUBMIN_GIT_DIR:-/submin/git}"
 admin_mail="${SUBMIN_ADMIN_MAIL:-root@submin.local}"
 
-if [ ! -e ${data_dir} ]; then
-    echo -e "svn\n${svn_repo}\n${hostname}:${external_port}\n\n\n" \
-        | submin2-admin ${data_dir} initenv ${admin_mail} >/dev/null
+
+echo -e "svn\n${svn_repo}\n${hostname}:${external_port}\n\n\n" \
+	| submin2-admin ${data_dir} initenv ${admin_mail} >/dev/null
 
 
-    submin2-admin ${data_dir} apacheconf create all >/dev/null 2>&1 || true
+submin2-admin ${data_dir} apacheconf create all >/dev/null 2>&1 || true
 
-    ln -s ${data_dir}/conf/apache-webui-cgi.conf /etc/httpd/conf.d/
-    ln -s ${data_dir}/conf/apache-svn.conf /etc/httpd/conf.d/
+ln -s ${data_dir}/conf/apache-webui-cgi.conf /etc/httpd/conf.d/
+ln -s ${data_dir}/conf/apache-svn.conf /etc/httpd/conf.d/
 
-    chmod 755  ${data_dir}/../
-    chown apache:apache -R ${data_dir}
-    chown apache:apache -R ${svn_repo}
+chmod 755  ${data_dir}/../
+chown apache:apache -R ${data_dir}
+chown apache:apache -R ${svn_repo}
 
-    submin2-admin ${data_dir} config set commit_email_from "Submin <submin@${hostname}>"
+submin2-admin ${data_dir} config set commit_email_from "Submin <submin@${hostname}>"
 
-    submin2-admin ${data_dir} config set smtp_from "Submin <submin@${hostname}>"
+submin2-admin ${data_dir} config set smtp_from "Submin <submin@${hostname}>"
 
-    # disable git
-    submin2-admin ${data_dir} config set vcs_plugins svn || true
+# disable git
+submin2-admin ${data_dir} config set vcs_plugins svn || true
 
-    submin2-admin ${data_dir} config set git_dir ${git_repo}
+submin2-admin ${data_dir} config set git_dir ${git_repo}
 
-    echo -e "git\n${hostname}\n127.0.0.1\n22\n" \
-        | submin2-admin ${data_dir} git init > /dev/null
+echo -e "git\n${hostname}\n127.0.0.1\n22\n" \
+	| submin2-admin ${data_dir} git init > /dev/null
 
-    key=`echo "SELECT key FROM password_reset;" | sqlite3 ${data_dir}/conf/submin.db`
-    echo "access http://${hostname}:${external_port}/submin/password/admin/${key} to reset password"
+key=`echo "SELECT key FROM password_reset;" | sqlite3 ${data_dir}/conf/submin.db`
+echo "access http://${hostname}:${external_port}/submin/password/admin/${key} to reset password"
 
-else
-    echo "Submin is already configured in ${data_dir}/conf"
-fi
 service httpd restart
 /usr/sbin/sshd
 /usr/sbin/sshd -D
